d2cdf79a62b37068201e31c70740aeec850545c7
daqian.zhang <daqian.zhang@g42.ai>
2023-02-03 00:17:33 +0400

    remove comment

--- misc_tests/jsoniter_decapalize_test.go
+++ misc_tests/jsoniter_decapalize_test.go
@@ -1,7 +1,6 @@
 package misc_tests
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/daqiancode/jsoniter"
@@ -35,23 +34,4 @@ func Test_Snake(t *testing.T) {
 	json.Unmarshal(bs, &s1)
 	require.Equal(t, s1.Field, s.Field)
 	require.Equal(t, s1.MyAddr, s.MyAddr)
-	fmt.Println(string(bs))
 }
-
-// func Test_s(t *testing.T) {
-// 	fmt.Println(snake("CamelCase"))
-// 	fmt.Println(snake("camelCamelCase"))
-// 	fmt.Println(snake("CCamelCCamelCase"))
-// 	fmt.Println(snake("CamelCamelCase"))
-// 	fmt.Println(snake("A"))
-// 	fmt.Println(snake("a"))
-// 	fmt.Println(snake("HTTPId"))
-// 	fmt.Println(snake("HTTP2Id"))
-// 	// snake("camelCamelCase")
-// 	// snake("CCamelCCamelCase")
-// 	// snake("CamelCamelCase")
-// 	// snake("aB")
-// 	// snake("A")
-// 	// snake("a")
-// 	// snake("HTTPId")
-// }
